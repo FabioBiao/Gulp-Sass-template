149c7c39b719188280d39b6ef879838cd933555c
FabioBiao <fabio_madeira@hotmail.com>
2017-10-05 20:38:32 +0100

    removed useless files
    removed useless files

--- js - Copy/randomscript.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function(){
-	console.log('so para testar o concact');
-	console.log(jsteste(4));
-});
-
-
-function jsteste(num){
-	return num +1;
-}
--- js - Copy/somescript.js
+++
@@ -1,9 +0,0 @@
-
-var teste = 'mais um teste em js'
-var num = 2;
-window.onload = function() { console.log('lalaa'); }
-
-$( document ).ready(function(){
-	console.log(teste);
-	console.log(num+2);
-});
